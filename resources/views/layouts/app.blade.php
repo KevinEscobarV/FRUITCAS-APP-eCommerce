543ffcfb7ab96344357a977ebd686bedf67ffc32
Kevin David Escobar Villegas <escobarkevin567@gmail.com>
2021-09-10 03:53:07 -0500

    bot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
     <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
     <script>
         var botmanWidget = {
-            // headers: {'X-CSRF-TOKEN': "{{ csrf_token() }}"},
+            headers: {'X-CSRF-TOKEN': "{{ csrf_token() }}"},
             title: 'ChatBot FruitCas',
             aboutText: 'Fruitcas',
             placeholderText: 'Envia un mensaje al Bot',
