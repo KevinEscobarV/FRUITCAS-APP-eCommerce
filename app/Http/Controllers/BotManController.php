93e069da7cf7fe79ffbe23fa63e0326d70caa717
Kevin David Escobar Villegas <escobarkevin567@gmail.com>
2021-09-13 06:38:04 -0500

    secure

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -14,12 +14,30 @@ class BotManController extends Controller
     public function handle()
     {
         $botman = app('botman');
-
-        $botman->hears('{message}', function ($botman, $message) {
-
-            $botman->startConversation(new OnboardingConversation);
+  
+        $botman->hears('{message}', function($botman, $message) {
+  
+            if ($message == 'hi') {
+                $this->askName($botman);
+            }else{
+                $botman->reply("write 'hi' for testing...");
+            }
+  
         });
-
+  
         $botman->listen();
+    }
+  
+    /**
+     * Place your BotMan logic here.
+     */
+    public function askName($botman)
+    {
+        $botman->ask('Hello! What is your Name?', function(Answer $answer) {
+  
+            $name = $answer->getText();
+  
+            $this->say('Nice to meet you '.$name);
+        });
     }
 }
